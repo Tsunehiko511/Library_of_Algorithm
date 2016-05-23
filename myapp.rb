c0cd2a8aa16587a32c5a970ade2a04baf8e92379
Tunehiko511 <simanezumi1989@gmail.com>
2016-05-24 04:02:57 +0900

    追加 実行コードの背景色変更

--- myapp.rb
+++ myapp.rb
@@ -1,8 +1,21 @@
+require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-    @title = 'Library of Algorithm'
-    @subtitle = 'Algorithm is fun!!!'
-    erb :index
+	@title = 'Library of Algorithm'
+	@subtitle = 'Algorithm is fun!!!'
+	erb :index
+end
+
+get '/def_kmean' do
+	erb :def_kmean
+end
+
+get '/anime_kmean' do
+	erb :anime_kmean
+end
+
+get '/que_kmean' do
+	erb :que_kmean
 end
--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,17 @@
-function main(){
-	var x = 1;
+function startAnimation() {
+	//「#obj」を「0.5」秒かけて「left」方向に「200」px アニメーションさせる
+	TweenMax.to('#obj', 1.5, { left: 200});
+}
+
+var count = 1;
+function color_line(){
+	if(count > 1){
+		var num = count -1;
+		document.getElementById('code'+ num).style.backgroundColor = 'transparent';
+	}
+	if(count>13){
+		count = 1;
+	}
+	document.getElementById('code'+ count).style.backgroundColor = '#FEFEC8';
+	count += 1;
 }
