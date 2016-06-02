6181e7305567c7b33ffdbc093753ba1d618bc17f
Tunehiko511 <simanezumi1989@gmail.com>
2016-06-02 18:26:19 +0900

    アンケートフォーム作成

--- public/js/ga_nap_anime.js
+++ public/js/ga_nap_anime.js
@@ -1,93 +1,90 @@
-// setColorLine(1);
+(function(){
+	const PNG_NUMBER = 21
+	var editor = ace.edit("editor");
+	var photo = document.getElementById('ga_nap_png');
+	var img = getImgFile("/img/ga_nap_anime/ga_nap_anime.", PNG_NUMBER)
+	//画像番号用のグローバル変数
+	var cnt=1;
 
-var editor = ace.edit("editor");
-
-//画像を配列に格納する
-var img = new Array();
-const PNG_L = 21
-img[0] = new Image();
-img[0].src = "/img/ga_nap_anime/ga_nap_anime.1.png";
-for(var i=1; i<=PNG_L;i++){
-	img[i] = new Image();
-	img[i].src = "/img/ga_nap_anime/ga_nap_anime."+i+".png";
-}
-/*img[16] = new Image();
-img[16].src = "/img/ga_nap/ga_nap_anime.15.png";
-*/
-//画像番号用のグローバル変数
-var cnt=1;
-
-//画像切り替え関数
-function changeIMG(){
-  //画像番号を進める
-  if (cnt == PNG_L){
-  	cnt=1;
-  }
-  else{
-  	cnt++;
-  }
-  //画像を切り替える
-  document.getElementById("ga_nap_png").src=img[cnt].src;
-}
-
-function setCodeLine(){
-	var startline = getCodeHighlight(cnt)[0];
-	var endline = getCodeHighlight(cnt)[1];
-	var range = editor.getSession().highlightLines(startline, endline, "code_highlight");
-	console.log(range.id);
-	if(range.id>3){
-		editor.getSession().removeMarker(range.id-1);
+	// タッチスクリーンなら
+	if (window.ontouchstart===null){
+		// 素早くタップしたときにダブルタップとみなされて拡大されるのを防ぐ
+		photo.addEventListener('touchstart',function(e){ e.preventDefault(); changeIMG();setCodeLine();},false);
 	}
-}
-function setColorLine(n){
-	if(n<-1){
-		return 0;
+
+	// タッチスクリーンでないなら
+	else{
+		// 'click'(=onClick)を使わないのは、素早くクリックしたときにダブルクリックとみなされて画面が選択されるのを防ぐため
+		photo.addEventListener('mousedown',function(e){ e.preventDefault();},false);
+		photo.addEventListener('mouseup',changeIMG,false);
+		photo.addEventListener('mouseup',setCodeLine,false);
 	}
-	document.getElementById('code'+ n).style.backgroundColor = 'Yellow';
-}
-function deleteColorLine(n){
-	if(n<-1){
-		return 0;
+
+	setCodeLine();
+
+
+	function getImgFile(path, number){
+		var img = new Array();
+		img[0] = new Image();
+		img[0].src = path+"1.png";
+		for(var i=1; i<=number;i++){
+			img[i] = new Image();
+			img[i].src = path+i+".png";
+		}	
+		return img
 	}
-	document.getElementById('code'+ n).style.backgroundColor = 'transparent';
-}
-function getCnt(){
-	return cnt;
-}
-setCodeLine();
-function getCodeHighlight(k){
-	if(k==1){
-		return [8,8];
-	}else if(k<=3){
-		return [12,13];
-	}else if(k==4){
-		return [15,15];
-	}else if(k==5){
-		return [12,13];
-	}else if(k==6){
-		return [18,18];
-	}else if(k==7){
-		return [19,19];
-	}else if(k<=10){
-		return [22,22];
-	}else if(k<=12){
-		return [23,24];
-	}else if(k<=13){
-		return [26,26];
-	}else if(k<=15){
-		return [28,30];
-	}else if(k<=16){
-		return [31,31];
-	}else if(k<=18){
-		return [34,35];
-	}else if(k==21){
-		return [38,38];
-	}else{
-		return [-1,-1];
+
+	//画像切り替え関数
+	function changeIMG(){
+	  //画像番号を進める
+	  if (cnt == PNG_NUMBER){
+	  	cnt=1;
+	  }
+	  else{
+	  	cnt++;
+	  }
+	  //画像を切り替える
+	  document.getElementById("ga_nap_png").src=img[cnt].src;
 	}
-}
 
+	function setCodeLine(){
+		var startline = getCodeHighlight(cnt)[0];
+		var endline = getCodeHighlight(cnt)[1];
+		var range = editor.getSession().highlightLines(startline, endline, "code_highlight");
+		if(range.id>3){
+			editor.getSession().removeMarker(range.id-1);
+		}
+	}
 
-
-
-
+	function getCodeHighlight(k){
+		if(k==1){
+			return [8,8];
+		}else if(k<=3){
+			return [12,13];
+		}else if(k==4){
+			return [15,15];
+		}else if(k==5){
+			return [12,13];
+		}else if(k==6){
+			return [18,18];
+		}else if(k==7){
+			return [19,19];
+		}else if(k<=10){
+			return [22,22];
+		}else if(k<=12){
+			return [23,24];
+		}else if(k<=13){
+			return [26,26];
+		}else if(k<=15){
+			return [28,30];
+		}else if(k<=16){
+			return [31,31];
+		}else if(k<=18){
+			return [34,35];
+		}else if(k==21){
+			return [38,38];
+		}else{
+			return [-1,-1];
+		}
+	}
+})();
---
+++ public/js/knn.js
@@ -0,0 +1,289 @@
+onload = function() {
+	draw_base();
+};
+
+function runLines(times){
+    for (var t = 0; t<=times;t++){
+        setTimeout("draw_line("+t+",1,'black')", t*250)
+    }
+}
+
+var count_anime = 0;
+function anime(k){
+
+	switch(count_anime){
+		case 0:
+			runLines(6);
+            setColorLine(1);
+			setColorLine(2);
+			count_anime += 1;
+			break;
+        case 1:
+            delet();
+            for (var t = 0; t<=6;t++){
+                draw_line(t,1,'black');
+            }
+            deleteColorLine(1)
+            deleteColorLine(2)
+            setColorLine(3);
+            count_anime += 1;
+            break;
+        case 2:
+            draw_neighbor(k);
+            deleteColorLine(3)
+            setColorLine(4);
+            count_anime += 1;
+            break;
+		// 数を数える(赤)
+		case 3:
+            delet();
+            draw_neighbor(k);
+            deleteColorLine(4)
+            setColorLine(5);
+            setColorLine(6);
+            draw_line(3,5,"orange");
+			count_anime += 1;
+			break;
+		case 4:
+            deleteColorLine(6);
+            setColorLine(7);
+			count_anime += 1;
+			break;
+		case 5:
+            delet();
+            draw_neighbor(k);
+            deleteColorLine(7);
+            setColorLine(6);
+            draw_line(2,5,"orange");
+			count_anime += 1;
+			break;
+		case 6:
+            deleteColorLine(6);
+            setColorLine(7);
+			count_anime += 1;
+			break;
+		case 7:
+            delet();
+            draw_neighbor(k);
+            deleteColorLine(7);
+            setColorLine(8);
+            draw_line(4,5,"orange");
+			count_anime += 1;
+			break;
+		case 8:
+            deleteColorLine(8);
+            setColorLine(9);
+            count_anime += 1;
+			break;
+		case 9:
+            deleteColorLine(5);
+            deleteColorLine(9);
+            setColorLine(10);
+			count_anime += 1;
+			break;
+		case 10:
+			if(k==3){
+				draw_fact("red");
+                deleteColorLine(10);
+                setColorLine(11);
+			}else if(k==5){
+				draw_fact("blue");
+                deleteColorLine(10);
+                setColorLine(13);
+			}
+			count_anime += 1;
+			break;
+        case 11:
+            deleteColorLine(11);
+            setColorLine(14);
+            count_anime += 1;
+            break;
+		default:
+			delet();
+			draw_base();
+			count_anime = 0;
+			color_lines(15);
+			break;
+	}
+}
+function delet(){
+    var canvas = document.getElementById("sample");
+    var context = canvas.getContext('2d');
+    context.clearRect(0,0,canvas.width,canvas.height);
+}
+
+function draw_base() {
+  //描画コンテキストの取得
+  var canvas = document.getElementById('sample');
+  if (canvas.getContext) {
+    var context = canvas.getContext('2d');
+    //ここに具体的な描画内容を指定する
+    context.beginPath();
+    context.arc(350,220, 15, 0, Math.PI*2, false);
+    context.fillStyle = "green";
+    context.fill();
+    //0
+    context.font= 'bold 20px Century Gothic';
+    context.beginPath();
+    context.arc(400,200, 15, 0, Math.PI*2, false);
+    context.fillStyle = "red";
+    context.fill();
+    context.fillStyle = "black";
+    context.fillText('a3,b3',400,185);
+    //1
+    context.beginPath();
+    context.arc(400,280, 15, 0, Math.PI*2, false);
+    context.fillStyle = "red";
+    context.fill();
+    context.fillStyle = "black";
+    context.fillText('a2,b2',400,265);
+    // 2
+    context.beginPath();
+    context.arc(270,280, 15, 0, Math.PI*2, false);
+    context.fillStyle = "blue";
+    context.fill();
+    context.fillStyle = "black";
+    context.fillText('a4,b4',270,265);
+    // 3
+    context.beginPath();
+    context.arc(210,240, 15, 0, Math.PI*2, false);
+    context.fillStyle = "blue";
+    context.fill();
+    context.fillStyle = "black";
+    context.fillText('a5,b5',210,225);
+    // 4
+    context.beginPath();
+    context.arc(300,60, 15, 0, Math.PI*2, false);
+    context.fillStyle = "blue";
+    context.fill();
+    context.fillStyle = "black";
+    context.fillText('a1,b1',300,45);
+    // 5
+    context.beginPath();
+    context.arc(400,30, 15, 0, Math.PI*2, false);
+    context.fillStyle = "red";
+    context.fill();
+    context.fillStyle = "black";
+    context.fillText('a0,b0',400,15);
+    // 6
+    context.beginPath();
+    context.arc(500,20, 15, 0, Math.PI*2, false);
+    context.fillStyle = "red";
+    context.fill();
+    context.fillStyle = "black";
+    context.fillText('a6,b6',500,45);
+    context.closePath();
+  }
+}
+function position(n){
+    switch(n){
+        case -1:
+        return {x:350,y:220};
+        case 0:
+        return {x:400,y:30};
+        case 1:
+        return {x:300,y:60};
+        case 2:
+        return {x:400,y:280};
+        case 3:
+        return {x:400,y:200};
+        case 4:
+        return {x:270,y:280};
+        case 5:
+        return {x:210,y:240};
+        case 6:
+        return {x:500,y:20};
+        default:
+        return {x:350,y:220};
+    }
+}
+
+
+function draw_line(n,width,color){
+    var canvas = document.getElementById('sample');
+    if (canvas.getContext) {
+        var context = canvas.getContext('2d');
+        context.beginPath();
+        context.lineWidth = width;
+        context.strokeStyle = color;
+        context.moveTo(position(-1).x,position(-1).y);
+        // 1
+        context.lineTo(position(n).x,position(n).y);
+        context.stroke();
+        context.closePath();
+    }
+    draw_base();
+}
+
+
+function draw_neighbor(k){
+  var canvas = document.getElementById('sample');
+  if (canvas.getContext) {
+    var context = canvas.getContext('2d');
+    //ここに具体的な描画内容を指定する
+    context.beginPath();
+    if(k==3){
+    	context.arc(350,220, 120, 0, Math.PI*2, false);
+    }else if(k==5){
+    	context.arc(350,220, 180, 0, Math.PI*2, false);    	
+    }
+    context.strokeStyle = "black";
+    context.lineWidth = 3;
+    context.fillText('k=3',450,120);
+    context.stroke();
+    context.closePath();
+  }
+}
+function draw_fact(color){
+  var canvas = document.getElementById('sample');
+  if (canvas.getContext) {
+    var context = canvas.getContext('2d');
+    //ここに具体的な描画内容を指定する
+    context.beginPath();
+    context.arc(350,220, 16, 0, Math.PI*2, false);
+    context.fillStyle = color;
+    context.fill();
+    context.closePath();
+  }
+}
+
+var count = 1;
+
+function setColorLine(n){
+    document.getElementById('code'+ n).style.backgroundColor = 'Yellow';
+}
+function deleteColorLine(n){
+    document.getElementById('code'+ n).style.backgroundColor = 'transparent';
+}
+
+function color_lines(times){
+	for(var i=times-1;i>0;i--){
+		document.getElementById('code'+ i).style.backgroundColor = 'transparent';
+	}
+	for(var i=times+1;i<14;i++){
+		document.getElementById('code'+ i).style.backgroundColor = 'transparent';
+	}
+
+	if(times<=14){
+		document.getElementById('code'+ times).style.backgroundColor = 'Yellow';
+	}
+}
+
+function color_line(){
+    for(var i=count-1;i>0;i--){
+        document.getElementById('code'+ i).style.backgroundColor = 'transparent';
+    }
+    for(var i=count+1;i<14;i++){
+        document.getElementById('code'+ i).style.backgroundColor = 'transparent';
+    }
+	if(count>14){
+		count = 1;
+	}
+	document.getElementById('code'+ count).style.backgroundColor = '#FEFEC8';
+	count += 1;
+}
+
+function start(){
+	color_line();
+	//setInterval("color_line()",1000);
+}
